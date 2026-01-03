9abfc497d624cf31533f1ab4d142c06a3e13d8b6
ReynardAdimas <dimastube22@gmail.com>
2026-01-03 16:41:06 +0700

    feat: add backend logic & API Construction

---
+++ backend/app/main.py
@@ -0,0 +1,47 @@
+import json
+import os
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from app.services.recommender import recommender_engine
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="IT Career Recommender API") 
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+class UserAnswer(BaseModel):
+    tags: List[str] 
+
+class JobRecommendation(BaseModel):
+    title: str
+    match_score: float
+    description: str
+
+
+@app.get("/")
+def read_root():
+    return {"status": "active", "message": "Welcome to IT Career Recommender API"}
+
+@app.get("/api/questions")
+def get_questions():
+    json_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "questions.json")
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        return data
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Questions file not found")
+
+@app.post("/api/recommend", response_model=List[JobRecommendation])
+def predict_career(payload: UserAnswer):
+    if not payload.tags:
+        raise HTTPException(status_code=400, detail="No tags provided")
+    
+    results = recommender_engine.get_recommendations(payload.tags)
+    return results
---
+++ backend/app/services/recommender.py
@@ -0,0 +1,56 @@
+import pickle
+import numpy as np
+import os
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+artifacts_dir = os.path.join(base_dir, "artifacts")
+
+class CareerRecommender:
+    def __init__(self):
+        self.df_jobs = None
+        self.job_embeddings = None
+        self.model = None
+        self._load_artifacts()
+
+    def _load_artifacts(self):
+        try:
+            with open(os.path.join(artifacts_dir, "jobs_data.pkl"), "rb") as f:
+                self.df_jobs = pickle.load(f)
+            
+            with open(os.path.join(artifacts_dir, "job_embedding.pkl"), "rb") as f:
+                self.job_embeddings = pickle.load(f)
+            
+            self.model = SentenceTransformer('all-MiniLM-L6-v2')
+            
+        except FileNotFoundError:
+            print("Artifacts Not Found")
+
+    def get_recommendations(self, user_tags: list[str], top_k=5):
+        query = " ".join(user_tags)
+        user_vector = self.model.encode([query])
+        scores = cosine_similarity(user_vector, self.job_embeddings)[0]
+        top_indices = np.argsort(scores)[::-1]
+        
+        recommendations = []
+        seen_roles = set()
+        
+        for idx in top_indices:
+            title = self.df_jobs.iloc[idx]['Job Title']
+            clean_title = title.lower().strip()
+            
+            if clean_title not in seen_roles:
+                recommendations.append({
+                    "title": title,
+                    "match_score": float(scores[idx]), 
+                    "description": str(self.df_jobs.iloc[idx]['Job Description'])[:200] + "..."
+                })
+                seen_roles.add(clean_title)
+            
+            if len(recommendations) >= top_k:
+                break
+                
+        return recommendations
+    
+recommender_engine = CareerRecommender()
